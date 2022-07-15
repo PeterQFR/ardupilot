cde1a0ca2e8e5d97f99e4c7409f08313cfd6e56d
peterQFR <petermilani80@gmail.com>
2022-07-15 18:15:04 +1000

    Add use JSON for sitl message passing

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-sim_vehicle.py -v ArduCopter -f gazebo-spurt --sim_vehicle_sh_compatible --load-module joystick
+sim_vehicle.py -v ArduCopter -f gazebo-spurt --model JSON --sim_vehicle_sh_compatible --load-module joystick
