25987aab5fbc8e33ab48118e808f3ce2b5a51a32
peterQFR <petermilani80@gmail.com>
2023-01-30 23:35:54 +1000

    Add documentation for configuring joystick

--- startup.sh
+++ startup.sh
@@ -1,1 +1,9 @@
 sim_vehicle.py -v ArduCopter -f gazebo-spurt --model JSON --sim_vehicle_sh_compatible --load-module joystick
+
+#if joystick isn't working check the defintion in your MAVProxy directory like:
+# /.local/lib/python3.8/site-packages/MAVProxy/modules/mavproxy_joystick/joysticks/taranis.yml 
+# ensure the name matches the joystick as found by the system
+#use the utilitiy below:
+
+#   python -m MAVProxy.modules.mavproxy_joystick.findjoy
+
