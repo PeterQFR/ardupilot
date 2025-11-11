38f08957c5ce2ce740026bd4eda2fd691e3343f6
peterQFR <petermilani80@gmail.com>
2025-11-11 13:01:29 +1000

    Remove console

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-sim_vehicle.py -v ArduCopter -f gazebo-spurt --model JSON --sim_vehicle_sh_compatible --load-module joystick --console
+sim_vehicle.py -v ArduCopter -f gazebo-spurt --model JSON --sim_vehicle_sh_compatible --load-module joystick 
 
 #if joystick isn't working check the defintion in your MAVProxy directory like:
 # /.local/lib/python3.8/site-packages/MAVProxy/modules/mavproxy_joystick/joysticks/taranis.yml 
